9d11d3b24e94d974a39ccdbd0b899872d4474c7b
Oğuzhan <oguzhanatasever93@gmail.com>
2022-08-05 14:24:25 +0300

    empty

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
-// bir tane array alacam
-// bir tane değer alacam
-// aldığım değer arrayin kaçıncı indexinde onu bulacam
-
-const indexBulan = (dizin, deger) => {
-  let bulunanIndex = "Bulunamadı";
-  for (let i = 0; i < dizin.length; i++) {
-    const element = dizin[i];
-    if (element === deger) {
-      bulunanIndex = i;
-      break;
-    }
-  }
-  return bulunanIndex;
-};
-
-const main = () => {
-  let benimDizinim = [1, 5, 2, 7, 9, 3, 6];
-  let aranacakSayi = 51;
-  const arananSayiIndexi = indexBulan(benimDizinim, aranacakSayi);
-  console.log("Aranan Sayi => ", aranacakSayi);
-  console.log("Sayinin Bulunduğu index => ", arananSayiIndexi);
-};
+const main = () => {};
 main();
