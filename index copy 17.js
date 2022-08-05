5a38334b74580a52d0751c3a28587b7325612a43
MeCelik <110526769+MeCelik@users.noreply.github.com>
2022-08-05 16:05:49 +0200

    test

--- index copy 17.js
+++ index copy 17.js
@@ -1,12 +1,12 @@
-function myFunction(num1, num2) {
+function myFunction(num1, num2, cb) {
   let sonuc = num1 + num2;
-  myFunctiontwo(sonuc);
+  cb(sonuc);
 }
 function myFunctiontwo(sonuc) {
   return sonuc;
 }
 
 const main = () => {
-  console.log(myFunction(19, 10));
+  console.log(myFunction(19, 10, myFunctiontwo));
 };
 main();
--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const main = () => {};
+const indexBulan = (dizin, deger) => {
+  let bulunanIndex = false;
+  for (let i = 0; i < dizin.length; i++) {
+    const element = dizin[i];
+    if (element === deger) {
+      bulunanIndex = i;
+      break;
+    }
+  }
+  return bulunanIndex;
+};
+
+const sayiTekYazdir = (dizin) => {
+  let tekSeferlikDizin = [];
+  for (let i = 0; i < dizin.length; i++) {
+    const element = dizin[i];
+    if (indexBulan(element) == false) {
+      tekSeferlikDizin.push(element);
+    }
+  }
+  console.log(tekSeferlikDizin);
+};
+
+const main = () => {
+  let sayiDizini = [5, 10, 14, 14, 20, 21, 23, 20];
+  sayiTekYazdir(sayiDizini);
+};
 main();
