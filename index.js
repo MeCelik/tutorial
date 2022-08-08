bc7a6f1c59aa858c73938d9eec921bd71dec56cf
Oğuzhan <oguzhanatasever93@gmail.com>
2022-08-08 21:31:07 +0300

    8.5

--- index.js
+++ index.js
@@ -8,8 +8,39 @@ const islem = (kucuk, buyuk) => {
   }
 };
 
+const islemUpdate = (kucuk, buyuk) => {
+  let bosDizin = [];
+  for (let i = kucuk; i <= buyuk; i++) {
+    if (i % 2 === 0) {
+      bosDizin.push(i);
+    }
+  }
+  return bosDizin;
+};
+
+const ciftleriBul = (kucuk, buyuk) => {
+  const ciftler = [];
+  for (let i = kucuk; i <= buyuk; i++) {
+    if (i % 2 === 0) {
+      ciftler.push(i);
+    }
+  }
+  return ciftler;
+};
+
 const main = () => {
   let sayilar = [5, 20];
+  const birinciSayi = 5;
+  const ikinciSayi = 20;
+  const ciftSayilar = ciftleriBul(birinciSayi, ikinciSayi);
+  console.log(
+    "Sayilar => ",
+    ciftSayilar,
+    "(Toplam ",
+    ciftSayilar.length,
+    " adet)"
+  );
+  console.log(islemUpdate(birinciSayi, ikinciSayi));
   console.log(islem(sayilar));
 };
 main();
