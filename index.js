8495e6c62861843434c231d48ba114c65ea3a992
Oğuzhan <oguzhanatasever93@gmail.com>
2022-08-08 17:02:41 +0300

    8.2

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const hesapla = (n) => {
+  let sonuc = 0;
+  for (let i = 1; i <= n; i++) {
+    sonuc = sonuc + (i + 1) / i;
+  }
+  return sonuc;
+};
+
 const islem1 = (sayi) => {
   let bosDizin = [];
   if (sayi > 0) {
@@ -17,5 +25,7 @@ const islem1 = (sayi) => {
 
 const main = () => {
   islem1(16);
+  const sonuc = hesapla(16);
+  console.log(sonuc);
 };
 main();
